b597df3605f8017e69ce5588bd6163c20fa43339
Ken Sawai <star.potatoes@gmail.com>
2016-08-09 18:58:45 +0900

    サインいん、ログイン完了

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,7 @@
 class PlacesController < ApplicationController
+
+  before_action :move_to_index, except: :index
+
   def index
     @places = Place.order("created_at DESC").page(params[:page]).per(3)
   end
@@ -13,6 +16,10 @@ end
 private
 def place_params
   params.permit(:name, :image, :url, :wifi, :outlet)
+end
+
+def move_to_index
+  redirect_to action: :index unless user_signed_in?
 end
 
 
