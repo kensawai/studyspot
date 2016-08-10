a820825d4d8134114108af3974fd23dda6caf0da
Ken Sawai <star.potatoes@gmail.com>
2016-08-10 14:34:39 +0900

    Userのコントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  def show
+    @nickname = current_user.nickname
+    @places = Place.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+end
+
+
+
+  end
