02f8a3c914ae856cc907ff7b186ea876744c6d5a
Ken Sawai <star.potatoes@gmail.com>
2016-08-10 14:54:51 +0900

    アソシエーション定義完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @nickname = current_user.nickname
-    @places = Place.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    @places = current_user.places.page(params[:page]).per(5).order("created_at DESC")
 end
 
 
